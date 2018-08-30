ec0e2592eae8968a47080b6b5282c82998a2654a
Malcolm Jones <malcolm@adobe.com>
2018-08-29 23:52:55 -0400

    wip: tap fix

--- Formula/cmu-sphinxbase.rb
+++ Formula/cmu-sphinxbase.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class CmuSphinxbase < Formula
+class CmuSphinxbaseAT5prealpha < Formula
   homepage "http://cmusphinx.sourceforge.net/"
   head "https://github.com/cmusphinx/sphinxbase.git"
 
@@ -15,7 +15,21 @@ class CmuSphinxbase < Formula
   depends_on "libsamplerate" => "with-libsndfile"
   depends_on "swig"
 
+  option "without-python", "Build without python 3 support"
+  option "with-python@2", "Build with python 2 support"
+
+  depends_on "python@2" => :optional
+  depends_on "python" => :recommended
+  depends_on "pygobject3" if build.with? "python"
+
   def install
+    if build.with?("python") && build.with?("python@2")
+      # Upstream does not support having both Python2 and Python3 versions
+      # of the plugin installed because apparently you can load only one
+      # per process, so GStreamer does not know which to load.
+      odie "You must pass both --without-python and --with-python@2 for python 2 support"
+    end
+
     system "./autogen.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
