1247f358c9943e6b5bedd1e3b578804eb4fe3c72
Malcolm Jones <malcolm@adobe.com>
2018-09-02 13:10:38 -0400

    wip; fix pyenv

--- pyenv_test.sh
+++ pyenv_test.sh
@@ -4,9 +4,9 @@
 
 # pyenv virtualenv 3.7.0 scarlett-deps37
 
-pyenv virtualenv --system-site-packages 3.7.0 scarlett-deps37
+# pyenv virtualenv --system-site-packages 3.7.0 scarlett-deps37
 
-pyenv activate scarlett-deps37
+# pyenv activate scarlett-deps37
 
 env PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig" pip install pygobject==3.28.3 ptpython black isort ipython pdbpp Pillow matplotlib numpy_ringbuffer MonkeyType
 
